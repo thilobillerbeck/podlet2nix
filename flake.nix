41805d839b4cb22eb2fabdcbe4ea7e7ee5f0bcd9
Thilo Billerbeck <thilo.billerbeck@officerent.de>
2026-01-30 00:44:20 +0100

    add flake-parts

--- flake.nix
+++ flake.nix
@@ -2,47 +2,59 @@
   description = "Nix flake for podlet2nix";
 
   inputs = {
+    flake-parts.url = "github:hercules-ci/flake-parts";
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
   };
 
   outputs =
-    {
-      nixpkgs,
-      flake-utils,
-      ...
-    }:
-    flake-utils.lib.eachDefaultSystem (
-      system:
-      let
-        pkgs = nixpkgs.legacyPackages.${system};
-
-        nativeBuildInputs = with pkgs; [
-          go
-          gopls
-          cobra-cli
-          podlet
-          (writeShellScriptBin "podlet2nix" ''
-            go run main.go "$@"
-          '')
-        ];
-      in
-      {
-        devShells.default = pkgs.mkShell { inherit nativeBuildInputs; };
-
-        packages.default = pkgs.buildGoModule {
-          name = "podlet2nix";
-          src = ./.;
-
-          cgo_enabled = false;
-
-          ldflags = [
-            "-s"
-            "-w"
+    inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      imports = [
+      ];
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "aarch64-darwin"
+        "x86_64-darwin"
+      ];
+      perSystem =
+        {
+          config,
+          self',
+          inputs',
+          pkgs,
+          system,
+          ...
+        }:
+        let
+          nativeBuildInputs = with pkgs; [
+            go
+            gopls
+            cobra-cli
+            podlet
+            (writeShellScriptBin "podlet2nix" ''
+              go run main.go "$@"
+            '')
           ];
+        in
+        {
+          devShells.default = pkgs.mkShell { inherit nativeBuildInputs; };
 
-          vendorHash = "sha256-wBdQIjRlA6Xr5o/ejmMO+/NWCGNKTApexb5z+6L7wbE=";
+          packages.default = pkgs.buildGoModule {
+            name = "podlet2nix";
+            src = ./.;
+
+            cgo_enabled = false;
+
+            ldflags = [
+              "-s"
+              "-w"
+            ];
+
+            vendorHash = "sha256-wBdQIjRlA6Xr5o/ejmMO+/NWCGNKTApexb5z+6L7wbE=";
+          };
         };
-      }
-    );
+      flake = {
+      };
+    };
 }
